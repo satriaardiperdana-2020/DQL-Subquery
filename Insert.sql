50e0d10c8b84c398d642c3657cd8c11b2f9f5256
Satria Ardi Perdana <55689358+satriaardiperdana-2020@users.noreply.github.com>
2024-04-24 11:10:42 +0700

    Create Insert.sql

---
+++ Insert.sql
@@ -0,0 +1,97 @@
+--pemrogramanbasisdata = nama database
+-- INSERT pegawai
+INSERT INTO pemrogramanbasisdata.pegawai
+(kd_pegawai, nm_pegawai, alamat, salary)
+VALUES(101, 'Rahmat', 'Jl. Kambing', 5000000);
+INSERT INTO pemrogramanbasisdata.pegawai
+(kd_pegawai, nm_pegawai, alamat, salary)
+VALUES(102, 'Salma', 'Jl. Landak', 6000000);
+INSERT INTO pemrogramanbasisdata.pegawai
+(kd_pegawai, nm_pegawai, alamat, salary)
+VALUES(103, 'Ridwan', 'Jl. Pisang', 5000000);
+INSERT INTO pemrogramanbasisdata.pegawai
+(kd_pegawai, nm_pegawai, alamat, salary)
+VALUES(104, 'Desi', 'Jl. NUsantara', 5000000);
+INSERT INTO pemrogramanbasisdata.pegawai
+(kd_pegawai, nm_pegawai, alamat, salary)
+VALUES(105, 'Tri', 'Jl. Sudirman', 7000000);
+INSERT INTO pemrogramanbasisdata.pegawai
+(kd_pegawai, nm_pegawai, alamat, salary)
+VALUES(106, 'Ken Ken', 'Jl. Buntu', 5000000);
+
+
+-- INSERT pegawai2
+
+INSERT INTO pemrogramanbasisdata.pegawai2
+(kd_pegawai, nm_pegawai, alamat, salary)
+VALUES(101, 'Rahmat', 'Jl. Kambing', 5000000);
+INSERT INTO pemrogramanbasisdata.pegawai2
+(kd_pegawai, nm_pegawai, alamat, salary)
+VALUES(103, 'Ridwan', 'Jl. Pisang', 5000000);
+INSERT INTO pemrogramanbasisdata.pegawai2
+(kd_pegawai, nm_pegawai, alamat, salary)
+VALUES(104, 'Desi', 'Jl. NUsantara', 5000000);
+INSERT INTO pemrogramanbasisdata.pegawai2
+(kd_pegawai, nm_pegawai, alamat, salary)
+VALUES(105, 'Tri', 'Jl. Sudirman', 5000000);
+INSERT INTO pemrogramanbasisdata.pegawai2
+(kd_pegawai, nm_pegawai, alamat, salary)
+VALUES(106, 'Ken Ken', 'Jl. Buntu', 5000000);
+
+-- INSERT pegawai3
+
+INSERT INTO pemrogramanbasisdata.pegawai3
+(nm_pegawai, alamat)
+VALUES('Rahmat', 'Jl. Kambing');
+INSERT INTO pemrogramanbasisdata.pegawai3
+(nm_pegawai, alamat)
+VALUES('Ridwan', 'Jl. Pisang');
+INSERT INTO pemrogramanbasisdata.pegawai3
+(nm_pegawai, alamat)
+VALUES('Desi', 'Jl. NUsantara');
+INSERT INTO pemrogramanbasisdata.pegawai3
+(nm_pegawai, alamat)
+VALUES('Tri', 'Jl. Sudirman');
+INSERT INTO pemrogramanbasisdata.pegawai3
+(nm_pegawai, alamat)
+VALUES('Ken Ken', 'Jl. Buntu');
+
+-- INSERT pegawai4
+INSERT INTO pemrogramanbasisdata.pegawai4
+(kd_pegawai, nm_pegawai, alamat, salary)
+VALUES(101, 'Rahmat', 'Jl. Kambing', 6000000);
+INSERT INTO pemrogramanbasisdata.pegawai4
+(kd_pegawai, nm_pegawai, alamat, salary)
+VALUES(102, 'Salma', 'Jl. Landak', 6000000);
+INSERT INTO pemrogramanbasisdata.pegawai4
+(kd_pegawai, nm_pegawai, alamat, salary)
+VALUES(103, 'Ridwan', 'Jl. Pisang', 6000000);
+INSERT INTO pemrogramanbasisdata.pegawai4
+(kd_pegawai, nm_pegawai, alamat, salary)
+VALUES(104, 'Desi', 'Jl. NUsantara', 6000000);
+INSERT INTO pemrogramanbasisdata.pegawai4
+(kd_pegawai, nm_pegawai, alamat, salary)
+VALUES(105, 'Tri', 'Jl. Sudirman', 6000000);
+INSERT INTO pemrogramanbasisdata.pegawai4
+(kd_pegawai, nm_pegawai, alamat, salary)
+VALUES(106, 'Ken Ken', 'Jl. Buntu', 6000000);
+
+
+-- INSERT pegawai4
+INSERT INTO pemrogramanbasisdata.pegawai5
+(kd_pegawai, nm_pegawai, alamat, salary)
+VALUES(101, 'Rahmat', 'Jl. Kambing', 5000000);
+INSERT INTO pemrogramanbasisdata.pegawai5
+(kd_pegawai, nm_pegawai, alamat, salary)
+VALUES(103, 'Ridwan', 'Jl. Pisang', 9000000);
+INSERT INTO pemrogramanbasisdata.pegawai5
+(kd_pegawai, nm_pegawai, alamat, salary)
+VALUES(104, 'Desi', 'Jl. NUsantara', 8000000);
+INSERT INTO pemrogramanbasisdata.pegawai5
+(kd_pegawai, nm_pegawai, alamat, salary)
+VALUES(105, 'Tri', 'Jl. Sudirman', 5400000);
+INSERT INTO pemrogramanbasisdata.pegawai5
+(kd_pegawai, nm_pegawai, alamat, salary)
+VALUES(106, 'Ken Ken', 'Jl. Buntu', 5500000);
+
+
